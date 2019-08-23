fcc9218d05d11d503e1d2cfe0a923998fcada47f
Panji Gumelar <panjiggm@gmail.com>
2019-08-23 17:58:00 +0700

    create Launch Component and show Query Data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
 import './App.css'
 
+import Launches from './components/Launches'
+
 const client = new ApolloClient({
   uri: 'http://localhost:9000/graphql'
 })
@@ -12,6 +14,7 @@ function App() {
     <ApolloProvider client={client}>
       <div className="container">
         <img src="/images/spacex.png" alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
+        <Launches />
       </div>
     </ApolloProvider>
   );
--- src/components/Launches.js
+++ src/components/Launches.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react';
-import { gql } from 'apollo-boost'
-import { useQuery } from '@apollo/react-hooks'
+import React, { Component, Fragment } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo'
+import LaunchItem from './LaunchItem'
+
+const LAUNCHES_QUERY = gql`
+   query LaunchesQuery {
+      launches {
+         flight_number
+         mission_name
+         launch_date_local
+         launch_success
+      }
+   }
+`
 
 class Launches extends Component {
    render() { 
       return ( 
-         <div></div>
+         <Fragment>
+            <h1 className="display-5 my-3">Launches</h1>
+            <Query query={LAUNCHES_QUERY}>
+               {({ loading, error, data }) => {
+                  if (loading) return <h4>Loading..</h4>
+                  if (error) console.log(`Error! ${error}`)
+
+                  return <Fragment>
+                     {
+                        data.launches.map(launch => (
+                           <LaunchItem key={launch.flight_number} launch={launch} />
+                        ))
+                     }
+                  </Fragment>
+               }}
+            </Query>
+         </Fragment>
        );
    }
 }
