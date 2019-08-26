67e5bb372c337e4391c51a6dc692afee27a977c3
Panji Gumelar <panjiggm@gmail.com>
2019-08-26 13:51:20 +0700

    coloing status, succes is green, fail is red

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css'
 
 import Launches from './components/Launches'
+import Launch from './components/Launch'
 
 const client = new ApolloClient({
   uri: 'http://localhost:9000/graphql'
@@ -12,10 +14,13 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="container">
-        <img src="/images/spacex.png" alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
-        <Launches />
-      </div>
+      <Router>
+        <div className="container">
+          <img src="/images/spacex.png" alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
+          <Route exact path="/" component={Launches} />
+          <Route path="/launch/:flight_number" component={Launch} />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
---
+++ src/components/Launch.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Launch extends Component {
+   render() { 
+      return ( 
+         <div>Launch</div>
+       );
+   }
+}
+ 
+export default Launch;
